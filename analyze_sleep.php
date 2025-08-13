67e67a11b589b9f6be5e16cf1313d059e6152366
Theodore Brown <theodorejb@outlook.com>
2025-08-13 13:33:50 -0500

    Gather additional __sleep and __wakeup stats

--- analyze_sleep.php
+++ analyze_sleep.php
@@ -17,6 +17,7 @@ $visitor = new class extends NodeVisitorAbstract {
     public string $code = '';
     public int $totalSleepMethods = 0;
     public int $totalWakeupMethods = 0;
+    public array $packages = [];
 
     public function enterNode(Node $node): void
     {
@@ -26,12 +27,61 @@ $visitor = new class extends NodeVisitorAbstract {
 
         $methodName = strtolower($node->name);
 
+        if ($methodName !== '__sleep' && $methodName !== '__wakeup') {
+            return;
+        }
+
+        $package = explode('\\', $this->path);
+        $package = implode('\\', array_slice($package, 0, 2));
+
+        if (!isset($this->packages[$package])) {
+            $this->packages[$package] = [
+                'Total sleep' => 0,
+                'Total wakeup' => 0,
+                'Empty sleep' => 0,
+                'Empty wakeup' => 0,
+                'Throw-only sleep' => 0,
+                'Throw-only wakeup' => 0,
+                'Other sleep' => 0,
+                'Other wakeup' => 0,
+            ];
+        }
+
+        $stmts = $node->stmts ?? [];
+        $stmts = array_filter($stmts, fn($node) => !$node instanceof Node\Stmt\Nop);
+        $stmts = array_values($stmts);
+        $numStmts = count($stmts);
+        $singleThrow = false;
+
+        if ($numStmts === 1) {
+            $stmt = $stmts[0];
+            if ($stmt instanceof Node\Stmt\Expression && $stmt->expr instanceof Node\Expr\Throw_) {
+                $singleThrow = true;
+            }
+        }
+
         if ($methodName === '__sleep') {
             $this->totalSleepMethods++;
-        } elseif ($methodName === '__wakeup') {
-            $this->totalWakeupMethods++;
+            $this->packages[$package]['Total sleep']++;
+
+            if ($numStmts === 0) {
+                $this->packages[$package]['Empty sleep']++;
+            } elseif ($singleThrow) {
+                $this->packages[$package]['Throw-only sleep']++;
+            } else {
+                $this->packages[$package]['Other sleep']++;
+            }
         } else {
-            return;
+            $this->totalWakeupMethods++;
+            $this->packages[$package]['Total wakeup']++;
+
+            if ($numStmts === 0) {
+                $this->packages[$package]['Empty wakeup']++;
+            } elseif ($singleThrow) {
+                $this->packages[$package]['Throw-only wakeup']++;
+            } else {
+                $this->packages[$package]['Other wakeup']++;
+            }
         }
 
         echo "{$this->path}:{$node->getStartLine()}\n";
@@ -49,8 +99,11 @@ $visitor = new class extends NodeVisitorAbstract {
 $traverser = new PhpParser\NodeTraverser;
 $traverser->addVisitor($visitor);
 
+$directory = __DIR__ . '/sources';
+$directoryLen = strlen($directory) + 1;
+
 $i = 0;
-foreach ($analyzer->getPhpFiles(__DIR__ . '/sources') as $path) {
+foreach ($analyzer->getPhpFiles($directory) as $path) {
     if (++$i % 1000 == 0) {
         echo $i . "\n";
     }
@@ -69,10 +122,40 @@ foreach ($analyzer->getPhpFiles(__DIR__ . '/sources') as $path) {
         continue;
     }
 
-    $visitor->path = $path;
+    $visitor->path = substr($path, $directoryLen);
     $visitor->code = $code;
     $traverser->traverse($stmts);
 }
 
 echo "Total __sleep methods: {$visitor->totalSleepMethods}\n";
 echo "Total __wakeup methods: {$visitor->totalWakeupMethods}\n";
+echo count($visitor->packages) . " distinct packages\n";
+
+$firstPackage = array_key_first($visitor->packages);
+$columns = array_keys($visitor->packages[$firstPackage]);
+
+echo "<table>
+<thead>
+<tr>
+    <th>Package</th>";
+
+foreach ($columns as $column) {
+    echo "    <th>{$column}</th>\n";
+}
+
+echo "
+</tr>
+</thead>
+<tbody>";
+
+foreach ($visitor->packages as $package => $info) {
+    echo "    <tr><td>" . htmlspecialchars($package) . "</td>";
+    foreach ($info as $value) {
+        echo "        <td>{$value}</td>\n";
+    }
+    echo "</tr>\n";
+}
+
+echo "
+</tbody></table>
+";
